b0b8d280fe6583a8ba4404539fe7e50cff6b6203
Jan Niehusmann <jan@gondor.com>
2021-03-03 22:21:14 +0000

    fix is_connected
    the value read from SetupAw is the raw register value, not the address length in bytes

--- src/lib.rs
+++ src/lib.rs
@@ -105,7 +105,7 @@ impl<E: Debug, CE: OutputPin<Error = E>, CSN: OutputPin<Error = E>, SPI: SpiTran
     /// Reads and validates content of the `SETUP_AW` register.
     pub fn is_connected(&mut self) -> Result<bool, Error<SPIE>> {
         let (_, setup_aw) = self.read_register::<SetupAw>()?;
-        let valid = setup_aw.aw() >= 3 && setup_aw.aw() <= 5;
+        let valid = setup_aw.aw() <= 3;
         Ok(valid)
     }
 }
